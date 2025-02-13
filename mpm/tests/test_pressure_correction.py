97079cc5dce3974ecb9c2873e1580819bd653f10
br00cew4yne <nncnep@gmail.com>
2025-02-13 14:28:13 +0100

    test: added more configurations to test on

--- mpm/tests/test_pressure_correction.py
+++ mpm/tests/test_pressure_correction.py
@@ -2,13 +2,14 @@ import utils  # import first to append parent directory to path
 
 from src.enums import Phase, Color, State, Classification
 from src.configurations import Configuration
+from src.geometries import Circle, Rectangle
 from src.mpm_solver import MPM_Solver
-from src.geometries import Square
 
 import taichi as ti
 import numpy as np
 
-ti.init(arch=ti.cpu)
+ti.init(arch=ti.cpu, debug=True)
+# ti.init(arch=ti.cuda, debug=True)
 
 
 @ti.kernel
@@ -80,38 +81,82 @@ def compute_divergence(solver: ti.template(), div: ti.types.ndarray()):  # pyrig
 
 
 def main() -> None:
-    configuration = Configuration(
-        name="Simple Spout Source (Water)",
-        geometries=[
-            # *[Square(Phase.Water, 0.05, 10, (0, -2), (0.45, 0.85), i) for i in range(10, 500)],
-            *[Square(Phase.Water, 0.05, 100, (0, -5.0), (0.45, 0.5), i) for i in range(10, 200)],
-        ],
-        E=1.4e5,  # Young's modulus (1.4e5)
-        nu=0.2,  # Poisson's ratio (0.2)
-        zeta=10,  # Hardening coefficient (10)
-        theta_c=2.5e-2,  # Critical compression (2.5e-2)
-        theta_s=5.0e-3,  # Critical stretch (7.5e-3)
-    )
+    configurations = [
+        Configuration(
+            name="Simple Spout Source (Water)",
+            geometries=[
+                *[Rectangle(Phase.Water, 0.05, 0.05, 10, (0, -2), (0.45, 0.85), i) for i in range(10, 500)],
+            ],
+            E=1.4e5,  # Young's modulus (1.4e5)
+            nu=0.2,  # Poisson's ratio (0.2)
+            zeta=10,  # Hardening coefficient (10)
+            theta_c=2.5e-2,  # Critical compression (2.5e-2)
+            theta_s=5.0e-3,  # Critical stretch (7.5e-3)
+        ),
+        Configuration(
+            name="Waterspout Hits Body of Water (Water)",
+            geometries=[
+                Rectangle(Phase.Water, 0.96, 0.1, 5_000, (0, 0), (0, 0)),
+                *[Rectangle(Phase.Water, 0.1, 0.05, 10, (0, -1), (0.45, 0.45), i) for i in range(10, 500)],
+            ],
+            E=1.4e5,  # Young's modulus (1.4e5)
+            nu=0.2,  # Poisson's ratio (0.2)
+            zeta=10,  # Hardening coefficient (10)
+            theta_c=2.5e-2,  # Critical compression (2.5e-2)
+            theta_s=5.0e-3,  # Critical stretch (7.5e-3)
+        ),
+        Configuration(
+            name="Waterspout Hits Ice Cubes (Water, Ice)",
+            geometries=[
+                *[Rectangle(Phase.Water, 0.05, 0.05, 25, (2, -2), (0.1, 0.8), i) for i in range(10, 500)],
+                Rectangle(Phase.Ice, 0.1, 0.1, 2000, (0, 0), (0.59, 0.0)),
+                Rectangle(Phase.Ice, 0.1, 0.1, 2000, (0, 0), (0.70, 0.0)),
+                Rectangle(Phase.Ice, 0.1, 0.1, 2000, (0, 0), (0.65, 0.1)),
+            ],
+            E=1.4e5,  # Young's modulus (1.4e5)
+            nu=0.1,  # Poisson's ratio (0.2)
+            zeta=20,  # Hardening coefficient (10)
+            theta_c=3.5e-2,  # Critical compression (2.5e-2)
+            theta_s=7.5e-3,  # Critical stretch (7.5e-3)
+        ),
+        Configuration(
+            name="Spherefall (Water)",
+            geometries=[
+                Circle(Phase.Water, 0.06, 4000, (0, 0), (0.5, 0.5)),
+            ],
+            E=1.4e5,  # Young's modulus (1.4e5)
+            nu=0.2,  # Poisson's ratio (0.2)
+            zeta=10,  # Hardening coefficient (10)
+            theta_c=2.5e-2,  # Critical compression (2.5e-2)
+            theta_s=5.0e-3,  # Critical stretch (7.5e-3)
+        ),
+    ]
 
-    solver = MPM_Solver(quality=1, max_particles=configuration.n_particles)
-    load_configuration(solver, configuration)
-    reset_solver(solver, configuration)
-
-    we_succeeded = True
+    max_particles = max([c.n_particles for c in configurations])
+    solver = MPM_Solver(quality=1, max_particles=max_particles)
     divergence = ti.ndarray(ti.f32, shape=(solver.n_grid, solver.n_grid))
-    for i in range(1, 501):
-        solver.substep()
-        compute_divergence(solver, divergence)
-        print(".", end=("\n" if i % 10 == 0 else " "), flush=True)
-        if np.any(divergence.to_numpy() > 0):
-            we_succeeded = False
+    we_succeeded = True
+
+    for configuration in configurations:
+        print(f"NOW RUNNING: {configuration.name}")
+        load_configuration(solver, configuration)
+        reset_solver(solver, configuration)
+        for i in range(1, 301):
+            solver.substep()
+            compute_divergence(solver, divergence)
+            print(".", end=("\n" if i % 10 == 0 else " "), flush=True)
+            if np.any(np.round(divergence.to_numpy(), 2) != 0):  # pyright: ignore
+                we_succeeded = False
+                break
+        if not we_succeeded:
             break
 
-    print()
-    print(divergence.to_numpy())
+    print("\n")
+    print("Divergence, min ->", np.min(divergence.to_numpy()))
+    print("Divergence, max ->", np.max(divergence.to_numpy()))
     print()
 
-    print(":)))))))))" if we_succeeded else ":(")
+    print("\033[92m:)))))))))\033[0m" if we_succeeded else "\033[91m:(\033[0m")
 
 
 if __name__ == "__main__":
