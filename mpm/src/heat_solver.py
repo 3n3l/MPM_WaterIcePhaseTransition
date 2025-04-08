2348933d69f2fe126840ad398365e677360b87dc
br00cew4yne <nncnep@gmail.com>
2025-04-08 13:40:47 +0200

    refactor: also renamed all variables in the heat solver

--- mpm/src/heat_solver.py
+++ mpm/src/heat_solver.py
@@ -14,26 +14,26 @@ class HeatSolver:
         self.dx = mpm_solver.dx
         self.dt = mpm_solver.dt
 
-        self.c_classification = mpm_solver.classification_c
-        self.c_temperature = mpm_solver.temperature_c
-        self.cell_capacity = mpm_solver.capacity_c
-        self.cell_mass = mpm_solver.mass_c
+        self.classification_c = mpm_solver.classification_c
+        self.temperature_c = mpm_solver.temperature_c
+        self.capacity_c = mpm_solver.capacity_c
+        self.mass_c = mpm_solver.mass_c
 
-        self.x_classification = mpm_solver.classification_x
-        self.y_classification = mpm_solver.classification_y
-        self.x_conductivity = mpm_solver.conductivity_x
-        self.y_conductivity = mpm_solver.conductivity_y
+        self.classification_x = mpm_solver.classification_x
+        self.classification_y = mpm_solver.classification_y
+        self.conductivity_x = mpm_solver.conductivity_x
+        self.conductivity_y = mpm_solver.conductivity_y
 
         self.should_use_direct_solver = should_use_direct_solver
 
     @ti.kernel
     def fill_linear_system(self, A: ti.types.sparse_matrix_builder(), T: ti.types.ndarray()):  # pyright: ignore
-        for i, j in self.c_temperature:
+        for i, j in self.temperature_c:
             # Raveled index.
             idx = (i * self.n_grid) + j
 
             # Set right-hand side to the cell temperature
-            T[idx] = self.c_temperature[i, j]
+            T[idx] = self.temperature_c[i, j]
 
             # FIXME: these variables are just used to print everything and can be removed after debugging
             A_t = 0.0
@@ -44,32 +44,32 @@ class HeatSolver:
 
             # We enforce Dirichlet temperature boundary conditions at CELLS that are in contact
             # with fixed temperature bodies (like a heated pan (-> colliding cells) or air (-> empty cells)).
-            if self.c_classification[i, j] == Classification.Interior:
-                inv_mass_capacity = 1 / (self.cell_mass[i, j] * self.cell_capacity[i, j])
+            if self.classification_c[i, j] == Classification.Interior:
+                inv_mass_capacity = 1 / (self.mass_c[i, j] * self.capacity_c[i, j])
                 A_c += 1.0
 
                 # We enforce homogeneous Neumann boundary conditions at FACES adjacent to
                 # cells that can be considered empty or corresponding to insulated objects.
                 # NOTE: dx^d is cancelled out by 1 / dx^2 because d == 2.
-                if i != 0 and self.c_classification[i - 1, j] != Classification.Empty:
-                    A[idx, idx - self.n_grid] -= self.dt * inv_mass_capacity * self.x_conductivity[i, j]
-                    A_l -= self.dt * inv_mass_capacity * self.x_conductivity[i, j]
-                    A_c += self.dt * inv_mass_capacity * self.x_conductivity[i, j]
+                if i != 0 and self.classification_c[i - 1, j] != Classification.Empty:
+                    A[idx, idx - self.n_grid] -= self.dt * inv_mass_capacity * self.conductivity_x[i, j]
+                    A_l -= self.dt * inv_mass_capacity * self.conductivity_x[i, j]
+                    A_c += self.dt * inv_mass_capacity * self.conductivity_x[i, j]
 
-                if i != self.n_grid - 1 and self.c_classification[i + 1, j] != Classification.Empty:
-                    A[idx, idx + self.n_grid] -= self.dt * inv_mass_capacity * self.x_conductivity[i + 1, j]
-                    A_r -= self.dt * inv_mass_capacity * self.x_conductivity[i + 1, j]
-                    A_c += self.dt * inv_mass_capacity * self.x_conductivity[i + 1, j]
+                if i != self.n_grid - 1 and self.classification_c[i + 1, j] != Classification.Empty:
+                    A[idx, idx + self.n_grid] -= self.dt * inv_mass_capacity * self.conductivity_x[i + 1, j]
+                    A_r -= self.dt * inv_mass_capacity * self.conductivity_x[i + 1, j]
+                    A_c += self.dt * inv_mass_capacity * self.conductivity_x[i + 1, j]
 
-                if j != 0 and self.c_classification[i, j - 1] != Classification.Empty:
-                    A[idx, idx - 1] -= self.dt * inv_mass_capacity * self.y_conductivity[i, j]
-                    A_b -= self.dt * inv_mass_capacity * self.y_conductivity[i, j]
-                    A_c += self.dt * inv_mass_capacity * self.y_conductivity[i, j]
+                if j != 0 and self.classification_c[i, j - 1] != Classification.Empty:
+                    A[idx, idx - 1] -= self.dt * inv_mass_capacity * self.conductivity_y[i, j]
+                    A_b -= self.dt * inv_mass_capacity * self.conductivity_y[i, j]
+                    A_c += self.dt * inv_mass_capacity * self.conductivity_y[i, j]
 
-                if j != self.n_grid - 1 and self.c_classification[i, j + 1] != Classification.Empty:
-                    A[idx, idx + 1] -= self.dt * inv_mass_capacity * self.y_conductivity[i, j + 1]
-                    A_t -= self.dt * inv_mass_capacity * self.y_conductivity[i, j + 1]
-                    A_c += self.dt * inv_mass_capacity * self.y_conductivity[i, j + 1]
+                if j != self.n_grid - 1 and self.classification_c[i, j + 1] != Classification.Empty:
+                    A[idx, idx + 1] -= self.dt * inv_mass_capacity * self.conductivity_y[i, j + 1]
+                    A_t -= self.dt * inv_mass_capacity * self.conductivity_y[i, j + 1]
+                    A_c += self.dt * inv_mass_capacity * self.conductivity_y[i, j + 1]
 
                 A[idx, idx] += A_c
             else:  # Dirichlet boundary condition (not homogeneous)
@@ -77,28 +77,28 @@ class HeatSolver:
                 A_c = 1.0
 
             continue
-            if self.c_classification[i, j] != Classification.Interior:
+            if self.classification_c[i, j] != Classification.Interior:
                 continue
             # if not (cell_is_interior and not_adjacent_to_fixed_temperature):
             # if (T[idx] < 0 or T[idx] > 0 or T[idx] == 0):
             #     continue
             print("~" * 100)
             print()
-            if self.c_classification[i, j] == Classification.Interior:
+            if self.classification_c[i, j] == Classification.Interior:
                 print(f">>> INTERIOR, idx = {idx}, i = {i}, j = {j}")
-            elif self.c_classification[i, j] == Classification.Colliding:
+            elif self.classification_c[i, j] == Classification.Colliding:
                 print(f">>> COLLIDING, idx = {idx}, i = {i}, j = {j}")
             else:
                 print(f">>> EMPTY, idx = {idx}, i = {i}, j = {j}")
 
             if i != 0:
-                print("cell i - 1 is empty->", self.c_classification[i - 1, j] != Classification.Empty)
+                print("cell i - 1 is empty->", self.classification_c[i - 1, j] != Classification.Empty)
             if i != self.n_grid - 1:
-                print("cell i + 1 is empty->", self.c_classification[i + 1, j] != Classification.Empty)
+                print("cell i + 1 is empty->", self.classification_c[i + 1, j] != Classification.Empty)
             if j != 0:
-                print("cell i - 1 is empty->", self.c_classification[i, j - 1] != Classification.Empty)
+                print("cell i - 1 is empty->", self.classification_c[i, j - 1] != Classification.Empty)
             if j != self.n_grid - 1:
-                print("cell i + 1 is empty->", self.c_classification[i, j + 1] != Classification.Empty)
+                print("cell i + 1 is empty->", self.classification_c[i, j + 1] != Classification.Empty)
 
             print()
             print(f"A[{idx}, {idx} + 1]   ->", A_t)
@@ -108,19 +108,19 @@ class HeatSolver:
             print(f"A[{idx}, {idx} - 1]   ->", A_b)
 
             print()
-            print("m_c                    ->", self.cell_mass[i, j])
-            print("c_c                    ->", self.cell_capacity[i, j])
-            print("1 / m_c * c_c          ->", 1 / (self.cell_mass[i, j] * self.cell_capacity[i, j]))
+            print("m_c                    ->", self.mass_c[i, j])
+            print("c_c                    ->", self.capacity_c[i, j])
+            print("1 / m_c * c_c          ->", 1 / (self.mass_c[i, j] * self.capacity_c[i, j]))
 
             print()
-            print("x_conductivity[i, j]   ->", self.x_conductivity[i, j])
+            print("x_conductivity[i, j]   ->", self.conductivity_x[i, j])
             if i != self.n_grid - 1:
-                print("x_conductivity[i + 1, j] ->", self.x_conductivity[i + 1, j])
+                print("x_conductivity[i + 1, j] ->", self.conductivity_x[i + 1, j])
 
             print()
-            print("y_conductivity[i, j]   ->", self.y_conductivity[i, j])
+            print("y_conductivity[i, j]   ->", self.conductivity_y[i, j])
             if j != self.n_grid - 1:
-                print("y_conductivity[i, j + 1] ->", self.y_conductivity[i, j + 1])
+                print("y_conductivity[i, j + 1] ->", self.conductivity_y[i, j + 1])
 
             print()
             print(f"b[{idx}]                ->", T[idx])
@@ -128,8 +128,8 @@ class HeatSolver:
 
     @ti.kernel
     def fill_temperature_field(self, T: ti.types.ndarray()):  # pyright: ignore
-        for i, j in self.c_temperature:
-            self.c_temperature[i, j] = T[(i * self.n_grid) + j]
+        for i, j in self.temperature_c:
+            self.temperature_c[i, j] = T[(i * self.n_grid) + j]
 
     def solve(self):
         # TODO: max_num_triplets could be optimized to N * 5?
