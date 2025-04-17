1155c2fa43c530c07baea9ff3c4823c96c24c6de
br00cew4yne <nncnep@gmail.com>
2025-04-17 19:54:37 +0200

    chore: converted configurations/presets

--- mpm/src/presets.py
+++ mpm/src/presets.py
@@ -1,33 +1,26 @@
 from src.configurations import Circle, Rectangle, Configuration
 from src.constants import Phase
 
+# Width of the bounding box, TODO: transform points to coordinates in bounding box
+offset = 0.0234375
+
 configuration_list = [
-    # Configuration(
-    #     name="Melting Ice Cube",
-    #     geometries=[
-    #         Rectangle(Phase.Ice, 0.15, 0.15, 5000, (0, 0), (0.425, 0.0), 0, -10.0),
-    #     ],
-    #     E=1.4e5,  # Young's modulus (1.4e5)
-    #     nu=0.2,  # Poisson's ratio (0.2)
-    #     zeta=10,  # Hardening coefficient (10)
-    #     theta_c=8.5e-2,  # Critical compression (2.5e-2)
-    #     theta_s=5.0e-3,  # Critical stretch (7.5e-3)
-    #     ambient_temperature=500.0,
-    # ),
-    # Configuration(
-    #     name="Freezing Water Cube",
-    #     geometries=[
-    #         Rectangle(Phase.Water, 0.2, 0.2, 5000, (0, 0), (0.4, 0.0), 0, 10.0),
-    #     ],
-    #     E=1.4e5,  # Young's modulus (1.4e5)
-    #     nu=0.2,  # Poisson's ratio (0.2)
-    #     zeta=10,  # Hardening coefficient (10)
-    #     theta_c=8.5e-2,  # Critical compression (2.5e-2)
-    #     theta_s=5.0e-3,  # Critical stretch (7.5e-3)
-    #     ambient_temperature=-5000.0,
-    # ),
     Configuration(
-        name="Waterspout Hits Pool [Water]",
+        name="Melting Ice Cube [Ice -> Water]",
+        geometries=[
+            Rectangle(
+                phase=Phase.Ice, size=(0.15, 0.15), velocity=(0, 0), lower_left=(0.425, offset), temperature=-10.0
+            ),
+        ],
+        E=1.4e5,  # Young's modulus (1.4e5)
+        nu=0.2,  # Poisson's ratio (0.2)
+        zeta=10,  # Hardening coefficient (10)
+        theta_c=8.5e-2,  # Critical compression (2.5e-2)
+        theta_s=5.0e-3,  # Critical stretch (7.5e-3)
+        ambient_temperature=500.0,
+    ),
+    Configuration(
+        name="Waterspout Hits Body of Water [Water]",
         geometries=[
             Rectangle(
                 lower_left=(0.0, 0.0),
@@ -55,34 +48,6 @@ configuration_list = [
         theta_s=5.0e-3,  # Critical stretch (7.5e-3)
         ambient_temperature=20.0,
     ),
-    # Configuration(
-    #     name="Waterspout Hits Ice Cube",
-    #     geometries=[
-    #         *[Rectangle(Phase.Water, 0.02, 0.02, 20, (0, -2), (0.48, 0.55), i, 100000.0) for i in range(5, 300)],
-    #         Rectangle(Phase.Ice, 0.1, 0.1, 3000, (0, 0), (0.45, 0.0), 0, -0.5),
-    #     ],
-    #     E=1.4e5,  # Young's modulus (1.4e5)
-    #     nu=0.2,  # Poisson's ratio (0.2)
-    #     zeta=10,  # Hardening coefficient (10)
-    #     theta_c=8.5e-2,  # Critical compression (2.5e-2)
-    #     theta_s=5.0e-3,  # Critical stretch (7.5e-3)
-    #     ambient_temperature=10.0,
-    # ),
-    # Configuration(
-    #     name="Waterspout Hits Ice Cubes",
-    #     geometries=[
-    #         *[Rectangle(Phase.Water, 0.05, 0.05, 25, (2, -2), (0.1, 0.8), i, 500.0) for i in range(10, 250)],
-    #         Rectangle(Phase.Ice, 0.1, 0.1, 1_000, (0, 0), (0.59, 0.0), 0, -10.0),
-    #         Rectangle(Phase.Ice, 0.1, 0.1, 1_000, (0, 0), (0.70, 0.0), 0, -10.0),
-    #         Rectangle(Phase.Ice, 0.1, 0.1, 1_000, (0, 0), (0.65, 0.1), 0, -10.0),
-    #     ],
-    #     E=1.4e5,  # Young's modulus (1.4e5)
-    #     nu=0.1,  # Poisson's ratio (0.2)
-    #     zeta=20,  # Hardening coefficient (10)
-    #     theta_c=3.5e-2,  # Critical compression (2.5e-2)
-    #     theta_s=7.5e-3,  # Critical stretch (7.5e-3)
-    #     ambient_temperature=20.0,
-    # ),
     Configuration(
         name="Stationary Pool [Water]",
         geometries=[
@@ -101,55 +66,77 @@ configuration_list = [
         theta_s=5.0e-3,  # Critical stretch (7.5e-3)
         ambient_temperature=20.0,
     ),
-    # Configuration(
-    #     name="Dropping Ice Cubes Into Body of Water",
-    #     geometries=[
-    #         Rectangle(Phase.Water, 0.96, 0.1, 8_000, (0, 0), (0, 0), 0, 50.0),
-    #         Rectangle(Phase.Ice, 0.05, 0.05, 1_000, (0, -1), (0.25, 0.35), 10, -30.0),
-    #         Rectangle(Phase.Ice, 0.05, 0.05, 1_000, (0, -1), (0.45, 0.15), 20, -30.0),
-    #         Rectangle(Phase.Ice, 0.05, 0.05, 1_000, (0, -1), (0.65, 0.25), 30, -30.0),
-    #     ],
-    #     E=1.4e5,  # Young's modulus (1.4e5)
-    #     nu=0.2,  # Poisson's ratio (0.2)
-    #     zeta=10,  # Hardening coefficient (10)
-    #     theta_c=2.5e-2,  # Critical compression (2.5e-2)
-    #     theta_s=5.0e-3,  # Critical stretch (7.5e-3)
-    #     ambient_temperature=0.0,
-    # ),
-    # Configuration(
-    #     name="Freezing Lake",
-    #     geometries=[
-    #         Rectangle(Phase.Water, 0.96, 0.1, 20_000, (0, 0), (0, 0), 0, 1.0),
-    #     ],
-    #     E=1.4e5,  # Young's modulus (1.4e5)
-    #     nu=0.2,  # Poisson's ratio (0.2)
-    #     zeta=10,  # Hardening coefficient (10)
-    #     theta_c=2.5e-2,  # Critical compression (2.5e-2)
-    #     theta_s=5.0e-3,  # Critical stretch (7.5e-3)
-    #     ambient_temperature=-500.0,
-    # ),
-    # Configuration(
-    #     name="Freezing Waterspout",
-    #     geometries=[
-    #         *[Rectangle(Phase.Water, 0.05, 0.05, 10, (0, -2), (0.45, 0.85), i, 30.0) for i in range(10, 500)],
-    #     ],
-    #     E=1.4e5,  # Young's modulus (1.4e5)
-    #     nu=0.2,  # Poisson's ratio (0.2)
-    #     zeta=10,  # Hardening coefficient (10)
-    #     theta_c=2.5e-2,  # Critical compression (2.5e-2)
-    #     theta_s=5.0e-3,  # Critical stretch (7.5e-3)
-    #     ambient_temperature=-500.0,
-    # ),
     Configuration(
-        name="Simple Spout Source",
+        name="Dropping Ice Cubes Into Body of Water [Water, Ice]",
         geometries=[
             Rectangle(
                 phase=Phase.Water,
-                size=(0.04, 0.04),
-                velocity=(0, -3),
+                size=(0.96, 0.1),
+                velocity=(0, 0),
+                lower_left=(0, 0),
+                frame_threshold=0,
+                temperature=50.0,
+            ),
+            Rectangle(
+                phase=Phase.Ice,
+                size=(0.05, 0.05),
+                velocity=(0, -1),
+                lower_left=(0.25, 0.35),
+                frame_threshold=10,
+                temperature=-30.0,
+            ),
+            Rectangle(
+                phase=Phase.Ice,
+                size=(0.05, 0.05),
+                velocity=(0, -1),
+                lower_left=(0.45, 0.15),
+                frame_threshold=20,
+                temperature=-30.0,
+            ),
+            Rectangle(
+                phase=Phase.Ice,
+                size=(0.05, 0.05),
+                velocity=(0, -1),
+                lower_left=(0.65, 0.25),
+                frame_threshold=30,
+                temperature=-30.0,
+            ),
+        ],
+        E=1.4e5,  # Young's modulus (1.4e5)
+        nu=0.2,  # Poisson's ratio (0.2)
+        zeta=10,  # Hardening coefficient (10)
+        theta_c=2.5e-2,  # Critical compression (2.5e-2)
+        theta_s=5.0e-3,  # Critical stretch (7.5e-3)
+        ambient_temperature=0.0,
+    ),
+    Configuration(
+        name="Freezing Lake [Water -> Ice]",
+        geometries=[
+            Rectangle(
+                lower_left=(0.0, 0.0),
+                phase=Phase.Water,
+                temperature=20.0,
+                size=(1.0, 0.15),
+                velocity=(0, 0),
+            ),
+        ],
+        E=1.4e5,  # Young's modulus (1.4e5)
+        nu=0.2,  # Poisson's ratio (0.2)
+        zeta=10,  # Hardening coefficient (10)
+        theta_c=2.5e-2,  # Critical compression (2.5e-2)
+        theta_s=5.0e-3,  # Critical stretch (7.5e-3)
+        ambient_temperature=-500.0,
+    ),
+    Configuration(
+        name="Simple Spout Source [Water]",
+        geometries=[
+            Rectangle(
                 lower_left=(0.48, 0.48),
+                velocity=(0, -3),
+                size=(0.04, 0.04),
                 frame_threshold=i,
                 temperature=20.0,
+                phase=Phase.Water,
             )
             for i in range(1, 200)
         ],
@@ -160,18 +147,28 @@ configuration_list = [
         theta_s=5.0e-3,  # Critical stretch (7.5e-3)
         ambient_temperature=20.0,
     ),
-    # Configuration(
-    #     name="Simple Blob Source",
-    #     geometries=[
-    #         *[Circle(Phase.Ice, 0.05, 1000, (5, 0), (0.1, 0.5), i, -20.0) for i in range(0, 250, 25)],
-    #     ],
-    #     E=1.4e5,  # Young's modulus (1.4e5)
-    #     nu=0.25,  # Poisson's ratio (0.2)
-    #     zeta=10,  # Hardening coefficient (10)
-    #     theta_c=8.5e-2,  # Critical compression (2.5e-2)
-    #     theta_s=7.5e-3,  # Critical stretch (7.5e-3)
-    #     ambient_temperature=-20.0,
-    # ),
+    Configuration(
+        name="Simple Blob Source [Ice]",
+        geometries=[
+            *[
+                Circle(
+                    velocity=(5, 0),
+                    center=(0.25, 0.2 + (i * 0.005)),
+                    radius=0.05,
+                    temperature=20.0,
+                    frame_threshold=i,
+                    phase=Phase.Ice,
+                )
+                for i in range(10, 110, 25)
+            ],
+        ],
+        E=1.4e5,  # Young's modulus (1.4e5)
+        nu=0.25,  # Poisson's ratio (0.2)
+        zeta=10,  # Hardening coefficient (10)
+        theta_c=8.5e-2,  # Critical compression (2.5e-2)
+        theta_s=7.5e-3,  # Critical stretch (7.5e-3)
+        ambient_temperature=-20.0,
+    ),
     Configuration(
         name="Spherefall [Water]",
         geometries=[
@@ -207,3 +204,6 @@ configuration_list = [
         ambient_temperature=-20.0,
     ),
 ]
+
+# Sort by length in descending order:
+configuration_list.sort(key=lambda c: len(c.name), reverse=True)
