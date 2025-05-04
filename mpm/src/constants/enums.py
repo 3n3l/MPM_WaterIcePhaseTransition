210281bcb5ba1b20bb5997949788aca60fa63c4e
br00cew4yne <nncnep@gmail.com>
2025-05-04 21:06:48 +0200

    -- updated parameters, very unstable but somewhat working

--- mpm/src/constants/enums.py
+++ mpm/src/constants/enums.py
@@ -52,15 +52,31 @@ class State:
 # TODO: find good values for ice???
 # TODO: refactor this, maybe use variable values again???
 # FIXME: E = 1.4e3 -> collapse, E = 1.4e4 -> explosion
-_E = 1.4e5
-_nu = 0.2
+ice_E = 1.4e4
+ice_nu = 0.2
+# water_E = 5e5
+# water_nu = 0.45
+
+# import numpy as np
+
+# Compute max dt from parameters:
+# TODO: move this somewhere else
+# dx = 128.0
+# ice_rho = Density.Ice / 1000
+# water_rho = Density.Water / 1000
+# max_dt_ice = dx / np.sqrt((ice_E * (1 - ice_nu)) / (ice_rho * (1 + ice_nu) * (1 - 2 * ice_nu)))
+# max_dt_water = dx / np.sqrt((water_E * (1 - water_nu)) / (water_rho * (1 + water_nu) * (1 - 2 * water_nu)))
+# print(f"max dt ice, water = {max_dt_ice}, {max_dt_water}")
+# print(f"water mu = {water_E / (2 * (1 + water_nu))}")
 
 
 class Lambda:
     Water = 5e9  # TODO: this could be lower, and then saved into f32 field again?!
-    Ice = _E * _nu / ((1 + _nu) * (1 - 2 * _nu))
+    # Water = water_E * water_nu / ((1 + water_nu) * (1 - 2 * water_nu))
+    Ice = ice_E * ice_nu / ((1 + ice_nu) * (1 - 2 * ice_nu))
 
 
 class Mu:
     Water = 0
-    Ice = _E / (2 * (1 + _nu))
+    # Water = water_E / (2 * (1 + water_nu))
+    Ice = ice_E / (2 * (1 + ice_nu))
